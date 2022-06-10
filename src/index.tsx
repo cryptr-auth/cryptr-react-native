b4278fdb9a3ea4efd77a64f7814e750a5ae54a0d
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-06-10 16:12:31 +0200

    [Release] v0.2.1 CryptrUser and Metadata types

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 export { default as CryptrProvider } from './models/CryptrProvider';
 export { default as CryptrReducer } from './models/CryptrReducer';
 export { default as useCryptr } from './useCryptr';
-export { CryptrState, CryptrConfig } from './utils/interfaces';
+export {
+  CryptrConfig,
+  CryptrState,
+  CryptrUser,
+  MetadataType,
+} from './utils/interfaces';
 export { Sign, Locale, Region } from './utils/enums';
 export * from './components';
