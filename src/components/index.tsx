cedd67d4497eab21320632ad8d3678d7b629ad0e
Thibaud RENAUX <thibaud.renaux@gmail.com>
2023-07-03 12:14:31 +0200

    export new component in lib

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,8 +2,10 @@ import CryptrSsoGatewayButton from './CryptrSsoGatewayButton';
 import CryptrSsoSignInButton from './CryptrSsoSignInButton';
 import CryptrLogOutButton from './CryptrLogOutButton';
 import CryptrRefreshButton from './CryptrRefreshButton';
+import CryptrGatewayButton from './CryptrGatewayButton';
 
 export const RefreshButton = CryptrRefreshButton;
 export const LogOutButton = CryptrLogOutButton;
 export const SsoSignInButton = CryptrSsoSignInButton;
 export const SsoGatewayButton = CryptrSsoGatewayButton;
+export const GatewayButton = CryptrGatewayButton;
