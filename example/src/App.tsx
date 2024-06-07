8d7017b84170cbedde577f718b249062632083a1
Thibaud RENAUX <thibaud.renaux@gmail.com>
2024-06-07 18:15:11 +0200

    use env vars for example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,17 +2,31 @@ import * as React from 'react';
 import { CryptrProvider } from '@cryptr/cryptr-react-native';
 import type { CryptrConfig } from '../../src/utils/interfaces';
 import HomeScreen from './screens/HomeScreen';
+import {
+  REACT_APP_CRYPTR_ACCOUNT_DOMAIN,
+  REACT_APP_CRYPTR_CLIENT_ID,
+  REACT_APP_CRYPTR_SERVICE_URL,
+  REACT_APP_CRYPTR_AUDIENCE,
+  REACT_APP_CRYPTR_DEFAULT_REDIRECT_URI,
+  REACT_APP_CRYPTR_DEDICATED_SERVER,
+  REACT_APP_CRYPTR_NO_POPUP_NO_COOKIE,
+  REACT_APP_CRYPTR_DEFAULT_LOCALE,
+} from '@env';
 
 const config: CryptrConfig = {
-  accountDomain: 'account-domain',
-  clientId: 'client-id',
-  cryptrServiceUrl: 'https://cryptr-service.ul',
-  audience: 'cryptr://2024-app',
-  defaultRedirectUri: 'cryptr://2024-app',
-  dedicatedServer: true,
-  noPopupNoCookie: true,
-  defaultLocale: 'en',
+  accountDomain: REACT_APP_CRYPTR_ACCOUNT_DOMAIN,
+  clientId: REACT_APP_CRYPTR_CLIENT_ID,
+  cryptrServiceUrl: REACT_APP_CRYPTR_SERVICE_URL,
+  audience: REACT_APP_CRYPTR_AUDIENCE,
+  defaultRedirectUri: REACT_APP_CRYPTR_DEFAULT_REDIRECT_URI,
+  dedicatedServer: REACT_APP_CRYPTR_DEDICATED_SERVER,
+  noPopupNoCookie: REACT_APP_CRYPTR_NO_POPUP_NO_COOKIE,
+  defaultLocale: REACT_APP_CRYPTR_DEFAULT_LOCALE,
 };
+
+console.debug(config);
+console.debug(REACT_APP_CRYPTR_ACCOUNT_DOMAIN);
+console.debug(process.env);
 
 export default function App() {
   return (
