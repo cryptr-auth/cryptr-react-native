af5ab3fe9090142382d8e616f7a4509eed0f43f5
Thibaud RENAUX <thibaud.renaux@gmail.com>
2024-06-07 18:15:11 +0200

    use env vars for example

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
 const pak = require('../package.json');
-
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
     [
       'module-resolver',
       {
