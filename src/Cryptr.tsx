7465833cdeb7ac89c46c47bc5679cfe1285921bc
Thibaud RENAUX <thibaud.renaux@gmail.com>
2024-06-27 12:17:52 +0200

    ignore linking error

--- src/Cryptr.tsx
+++ src/Cryptr.tsx
@@ -1,11 +1,12 @@
-import { NativeModules, Platform } from 'react-native';
+// import { NativeModules, Platform } from 'react-native';
+import { NativeModules } from 'react-native';
 import type CryptrInterface from './utils/interfaces';
 
-const LINKING_ERROR =
-  `The package '@cryptr/cryptr-react-native' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+// const LINKING_ERROR =
+//   `The package '@cryptr/cryptr-react-native' doesn't seem to be linked. Make sure: \n\n` +
+//   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+//   '- You rebuilt the app after installing the package\n' +
+//   '- You are not using Expo Go\n';
 
 const CryptrReactNative = NativeModules.CryptrReactNative
   ? NativeModules.CryptrReactNative
@@ -13,7 +14,7 @@ const CryptrReactNative = NativeModules.CryptrReactNative
       {},
       {
         get() {
-          throw new Error(LINKING_ERROR);
+          // throw new Error(LINKING_ERROR);
         },
       }
     );
